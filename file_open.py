c24710f29a5617c292ff50e305248d4765b4af91
Nikolai Gusarov <Nikolai_polos@mail.ru>
2023-10-08 09:55:10 +0300

    Exercise №2

--- file_open.py
+++ file_open.py
@@ -15,5 +15,25 @@ def my_cook_book():
                           }
                       )
                 dishes[name] = ingredients_quantity
-        return f'cook_book = {dishes}'
-print(my_cook_book())
+        return dishes
+# print(my_cook_book())
+
+cook_book = my_cook_book()
+# print(type(cook_book))
+
+def get_shop_list_by_dishes(dishes, person_count):
+    counted_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for i in cook_book[dish]:
+                ingredient_name = i['ingredient_name']
+                quantity = i['quantity'] * person_count
+                if ingredient_name in counted_list:
+                    counted_list[ingredient_name]['quantity'] += quantity
+                else:
+                    counted_list[ingredient_name] = {'measure': i['measure'], 'quantity': quantity}
+
+    return counted_list
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
